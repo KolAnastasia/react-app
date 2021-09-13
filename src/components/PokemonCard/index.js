750bfa8f7ddd6fde151668791aadadacb15debc3
KolAnastasia <53018954+KolAnastasia@users.noreply.github.com>
2021-09-13 12:43:15 +0300

    remake

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -8,7 +8,7 @@ const PokemonCard = ({name, img, id, type, values}) => {
 
     const [isActive, setActive] = useState(false)
     const handleOpen = () => {
-        setActive(true)
+        setActive(!isActive)
     }
     
     return (
